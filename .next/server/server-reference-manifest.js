0ba96153362ad3ce74562c999241ceda24b40bcc
Mobile Dev <134917998+FreelanceMobileDev@users.noreply.github.com>
2024-07-05 17:43:00 +0530

    chnage

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"VIFMGs5pxAGqsAicJ7JxHZuefgVTGP1yI4oZSh0s5Eo=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"dhiQVc1XKAjumHI5eKNDor4EpYvRsTPjNyoH4OPozx4=\"}"
--- src/app/_components/ScheduleContant.js
+++ src/app/_components/ScheduleContant.js
@@ -1,83 +1,104 @@
 'use client';
-import { CalenderIcon, CashIcon, CheckIcon, ClockCalender, ClockIcon, CrossIcon, LeftArrow, PlayIcon, PlusIcon, Rightarrow, } from '../../../public';
+
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import { format, addDays, subDays, addWeeks, subWeeks } from 'date-fns';
 import OpticityButton from '../_reuseableComponent/OpicityButton';
-import styles from './Login.module.css';
-import React, { useState,useEffect } from 'react';
-import { format, addDays, subDays, addWeeks, subWeeks } from 'date-fns'
 import NewPayment from '../Popups/NewPayment';
 import NewTraining from '../Popups/NewTraining';
 import EditTraining from '../Popups/EditTraining';
-// 
-const ScheduleContant = () => {
+import styles from './Login.module.css';
+
+import {
+  CalenderIcon,
+  CashIcon,
+  CheckIcon,
+  ClockCalender,
+  ClockIcon,
+  CrossIcon,
+  LeftArrow,
+  PlayIcon,
+  PlusIcon,
+  Rightarrow,
+} from '../../../public';
+
+const ScheduleContent = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
-    const [selectedDate, setSelectedDate] = useState(new Date());
-    const [popupIsOpen, setShowPopup] = useState(false);
-    const [newTrainingpop, setNewTrainingpop] = useState(false);
-    const [editpopup, seteditpopup] = useState(false);
-    const openEditPopup = () => {
-      seteditpopup(true);
-    };
-  
-    const closeEditPopup = () => {
-      seteditpopup(false);
-    };
-    const openPopup = () => {
-      setShowPopup(true);
-    };
-  
-    const closePopup = () => {
-      setShowPopup(false);
-    };
-    const openTraning = () => {
-      setNewTrainingpop(true);
-    };
-  
-    const closeopenTraning = () => {
-      setNewTrainingpop(false);
-    };
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [popupIsOpen, setShowPopup] = useState(false);
+  const [newTrainingpop, setNewTrainingpop] = useState(false);
+  const [editpopup, seteditpopup] = useState(false);
 
-    useEffect(() => {
-        const centerSelectedDate = () => {
-            const selectedElement = document.querySelector(`.${styles.selected}`);
-            if (selectedElement) {
-                selectedElement.scrollIntoView({ behavior: 'smooth', inline: 'center' });
-            }
-        };
-        centerSelectedDate();
-    }, [selectedDate]);
+  const openEditPopup = () => {
+    seteditpopup(true);
+  };
 
- const handlePrevWeek = () => {
-        setCurrentDate(subWeeks(currentDate, 1));
+  const closeEditPopup = () => {
+    seteditpopup(false);
+  };
+
+  const openPopup = () => {
+    setShowPopup(true);
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+  };
+
+  const openTraning = () => {
+    setNewTrainingpop(true);
+  };
+
+  const closeopenTraning = () => {
+    setNewTrainingpop(false);
+  };
+
+  useEffect(() => {
+    const centerSelectedDate = () => {
+      const selectedElement = document.querySelector(`.${styles.selected}`);
+      if (selectedElement) {
+        selectedElement.scrollIntoView({ behavior: 'smooth', inline: 'center' });
+      }
     };
+    centerSelectedDate();
+  }, [selectedDate]);
 
-    const handleNextWeek = () => {
-        setCurrentDate(addWeeks(currentDate, 1));
-    };
+  const handlePrevWeek = () => {
+    setCurrentDate(subWeeks(currentDate, 1));
+  };
 
-    const handleDateClick = (date) => {
-        setSelectedDate(date);
-    };
+  const handleNextWeek = () => {
+    setCurrentDate(addWeeks(currentDate, 1));
+  };
 
-    const startOfWeek = subDays(currentDate, currentDate.getDay() - 1);
-    const endOfWeek = addDays(startOfWeek, 6);
-    const days = [];
-    for (let date = startOfWeek; date <= endOfWeek; date = addDays(date, 1)) {
-        days.push(new Date(date));
-    }
+  const handleDateClick = (date) => {
+    setSelectedDate(date);
+  };
+
+  const startOfWeek = subDays(currentDate, currentDate.getDay() - 1);
+  const endOfWeek = addDays(startOfWeek, 6);
+  const days = [];
+  for (let date = startOfWeek; date <= endOfWeek; date = addDays(date, 1)) {
+    days.push(new Date(date));
+  }
+
   const newClients = [
     { name: 'Eloise Robinson', avatar: '/images/profilepic.png' },
     { name: 'Franky Williamson', avatar: '/images/profilepic.png' },
     { name: 'Bronson Glass', avatar: '/images/profilepic.png' },
   ];
+
   const sessions = [
     { time: '10:00 AM', name: 'Gabe Woodward', backgroundColor: '#FFE0E0', avatar: '/images/profilepic.png', icon: <CrossIcon /> },
     { time: '11:00 AM', name: 'Academic Team', backgroundColor: '#E0F7FF', avatar: '/images/profilepic.png', avatar2: '/images/profilepic.png', icon: <PlayIcon /> },
     { time: '11:20 AM', name: "Samuel O'Brien", backgroundColor: '#E0FFE1', avatar: '/images/profilepic.png', icon: <CheckIcon /> },
   ];
+
   const sessionssecond = [
     { time: '11:00 AM', name: 'Academic Team', backgroundColor: '#E0EAFE', avatar: '/images/profilepic.png', avatar2: '/images/profilepic.png', icon: <PlayIcon /> },
     { time: '11:20 AM', name: "Samuel O'Brien", backgroundColor: '#E0FFE1', avatar: '/images/profilepic.png', icon: <CheckIcon /> },
   ];
+
   const Augestdata = [
     { time: '11:00 AM', name: 'Academic Team', backgroundColor: '#E0F7FF', avatar: '/images/profilepic.png', icon: <ClockCalender /> },
   ];
@@ -85,56 +106,51 @@ const ScheduleContant = () => {
   return (
     <div className={styles.DashboardContent}>
       <div className={styles.summarySecedule}>
-      <div className={styles.calendarContainer}>
-            <div className={styles.headercalender}>
-                <div className={styles.monthname}>{format(currentDate, 'MMMM yyyy')}</div>
-                <div className={styles.buttons}>
-                    <button className={styles.actionButton} onClick={openPopup} >New Payment</button>
-                    <button className={styles.actionButton} onClick={openTraning}>Schedule a Training</button>
-                </div>
+        <div className={styles.calendarContainer}>
+          <div className={styles.headercalender}>
+            <div className={styles.monthname}>{format(currentDate, 'MMMM yyyy')}</div>
+            <div className={styles.buttons}>
+              <button className={styles.actionButton} onClick={openPopup}>New Payment</button>
+              <button className={styles.actionButton} onClick={openTraning}>Schedule a Training</button>
             </div>
-            <div className={styles.navigation}>
-              <div onClick={handlePrevWeek}>
-                <LeftArrow/>
-                </div>
-               
-                <div className={styles.weekContainer}>
-                    {days.map((date) => (
-                        <div
-                            key={date}
-                            className={`${styles.day} ${date.toDateString() === selectedDate.toDateString() ? styles.selected : ''}`}
-                            onClick={() => handleDateClick(date)}
-                        >
-                            <div className={styles.date} style={{ color: date.toDateString() === selectedDate.toDateString() ? 'white' : '#697585' }}>{format(date, 'd')}</div>
-                            <div className={styles.label} style={{ color: date.toDateString() === selectedDate.toDateString() ? 'white' : '#697585' }}>{format(date, 'EEE')}</div>
-                        </div>
-                    ))}
-                </div>
-                <div onClick={handleNextWeek}>
-                  <Rightarrow/>
-                </div>
-                {/* <button className={styles.navButton} onClick={handleNextWeek}>&gt;</button> */}
+          </div>
+          <div className={styles.navigation}>
+            <div onClick={handlePrevWeek}>
+              <LeftArrow />
             </div>
+            <div className={styles.weekContainer}>
+              {days.map((date) => (
+                <div
+                  key={date}
+                  className={`${styles.day} ${date.toDateString() === selectedDate.toDateString() ? styles.selected : ''}`}
+                  onClick={() => handleDateClick(date)}
+                >
+                  <div className={styles.date} style={{ color: date.toDateString() === selectedDate.toDateString() ? 'white' : '#697585' }}>{format(date, 'd')}</div>
+                  <div className={styles.label} style={{ color: date.toDateString() === selectedDate.toDateString() ? 'white' : '#697585' }}>{format(date, 'EEE')}</div>
+                </div>
+              ))}
+            </div>
+            <div onClick={handleNextWeek}>
+              <Rightarrow />
+            </div>
+          </div>
         </div>
 
-       {popupIsOpen && <NewPayment show={popupIsOpen} handleClose={closePopup} />}
-
-{newTrainingpop&& <NewTraining show={newTrainingpop} handleClose={closeopenTraning}/>}
-{/* editpopup */}
-{editpopup && <EditTraining show={editpopup} handleClose={closeEditPopup}/> }
+        {popupIsOpen && <NewPayment show={popupIsOpen} handleClose={closePopup} />}
+        {newTrainingpop && <NewTraining show={newTrainingpop} handleClose={closeopenTraning} />}
+        {editpopup && <EditTraining show={editpopup} handleClose={closeEditPopup} />}
 
         <div className={styles.session}>
           <div className={styles.TodayTxtdiv}>
             <span>Today</span>
             <PlusIcon />
           </div>
-
           <div className={styles.sessionDetails}>
             {sessions.map((session, index) => (
-              <div className={styles.sessionCard2} >
+              <div className={styles.sessionCard2} key={index}>
                 <p style={{ marginBottom: 20, width: 120 }}>{session.time}</p>
-                <div key={index} className={styles.sessionCard} style={{ backgroundColor: session.backgroundColor }}>
-                  <div style={{ display: 'flex', alignItems: 'center', }}>
+                <div className={styles.sessionCard} style={{ backgroundColor: session.backgroundColor }}>
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
                     {session.icon}
                     <img src={session.avatar} className={styles.avatarimagee} />
                     {session.avatar2 && <img src={session.avatar2} className={styles.avatar2imagee} />}
@@ -145,8 +161,8 @@ const ScheduleContant = () => {
               </div>
             ))}
           </div>
-
         </div>
+
         <div className={styles.session}>
           <div className={styles.TodayTxtdiv}>
             <span>August 18</span>
@@ -154,6 +170,7 @@ const ScheduleContant = () => {
           </div>
           <p>No Trainings</p>
         </div>
+
         <div className={styles.session}>
           <div className={styles.TodayTxtdiv}>
             <span>August 18</span>
@@ -161,10 +178,10 @@ const ScheduleContant = () => {
           </div>
           <div className={styles.sessionDetails}>
             {Augestdata.map((session, index) => (
-              <div className={styles.sessionCard2} >
+              <div className={styles.sessionCard2} key={index}>
                 <p style={{ marginBottom: 20, width: 120 }}>{session.time}</p>
-                <div key={index} className={styles.sessionCard} style={{ backgroundColor: session.backgroundColor }}>
-                  <div style={{ display: 'flex', alignItems: 'center', }}>
+                <div className={styles.sessionCard} style={{ backgroundColor: session.backgroundColor }}>
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
                     {session.icon}
                     <img src={session.avatar} className={styles.avatarimagee} />
                     <p style={{ marginLeft: 10 }}>{session.name}</p>
@@ -175,18 +192,18 @@ const ScheduleContant = () => {
             ))}
           </div>
         </div>
+
         <div className={styles.session}>
           <div className={styles.TodayTxtdiv}>
             <span>Today</span>
             <PlusIcon />
           </div>
-
           <div className={styles.sessionDetails}>
             {sessionssecond.map((session, index) => (
-              <div className={styles.sessionCard2} >
+              <div className={styles.sessionCard2} key={index}>
                 <p style={{ marginBottom: 20, width: 120 }}>{session.time}</p>
-                <div key={index} className={styles.sessionCard} style={{ backgroundColor: session.backgroundColor }}>
-                  <div style={{ display: 'flex', alignItems: 'center', }}>
+                <div className={styles.sessionCard} style={{ backgroundColor: session.backgroundColor }}>
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
                     {session.icon}
                     <img src={session.avatar} className={styles.avatarimagee} />
                     {session.avatar2 && <img src={session.avatar2} className={styles.avatar2imagee} />}
@@ -197,9 +214,9 @@ const ScheduleContant = () => {
               </div>
             ))}
           </div>
-
         </div>
       </div>
+
       <div className={styles.newclientSidebar} style={{ marginTop: 30 }}>
         <div className={styles.space_between}>
           <div />
@@ -217,48 +234,36 @@ const ScheduleContant = () => {
           <ClockIcon />
           <div className={styles.time_txt}>02:30 pm - 04:30 pm</div>
         </div>
-
         <div>
-<div className={styles.clientslist}>
-        Clients
+          <div className={styles.clientslist}>Clients</div>
+          {newClients.map((client, index) => (
+            <div key={index} className={styles.newclientItem}>
+              <img src={client.avatar} alt={client.name} className={styles.avatar} />
+              <span>{client.name}</span>
+            </div>
+          ))}
         </div>
-        
-            {newClients.map((client, index) => (
-              <div key={index} className={styles.newclientItem}>
-                <img src={client.avatar} alt={client.name} className={styles.avatar} />
-                <span>{client.name}</span>
-              </div>
-            ))}
-         
-          </div>
-          <div className={styles.clientslist}>
-          Comment
-        </div>
-        <div>
-        Aliquam pulvinar vestibulum blandit. Donec sed nisl libero.
-        </div>
-        <div className={styles.clientslist}>
-        Type of Payment
-        </div>
+        <div className={styles.clientslist}>Comment</div>
+        <div>Aliquam pulvinar vestibulum blandit. Donec sed nisl libero.</div>
+        <div className={styles.clientslist}>Type of Payment</div>
         <div className={styles.cashIcon_txt}>
-          <CashIcon/> <div style={{marginLeft:10}}>Cash</div>
+          <CashIcon />
+          <div style={{ marginLeft: 10 }}>Cash</div>
         </div>
         <OpticityButton
-        style={{backgroundColor:'#CFF3FD'}}
-        txtstyle={{color:'#14AED1'}}
-        name={"Edit"}
-        onClick={openEditPopup}
+          style={{ backgroundColor: '#CFF3FD' }}
+          txtstyle={{ color: '#14AED1' }}
+          name="Edit"
+          onClick={openEditPopup}
         />
-         <OpticityButton
-        style={{backgroundColor:'#FFE6D7'}}
-        txtstyle={{color:'#FE4726'}}
-        name={"Cancel"}
+        <OpticityButton
+          style={{ backgroundColor: '#FFE6D7' }}
+          txtstyle={{ color: '#FE4726' }}
+          name="Cancel"
         />
       </div>
-     
     </div>
+  );
+};
 
-  )
-}
-
-export default ScheduleContant
+export default ScheduleContent;
