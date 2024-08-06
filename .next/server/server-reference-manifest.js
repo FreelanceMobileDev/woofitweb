9460fb5e2fe866e789d87cd71064402db17663ae
developer2 <phpdev074@gmail.com>
2024-08-06 20:19:53 +0530

    add changes

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"lNDi7wDLDxEZpIKNPEd+RR5LM4BQKkwWYRv0c20qhpU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"O67e+hfWcEmHBaFY+6sdYgs+u1tKRbWltj0p5NUFkv0=\"}"
